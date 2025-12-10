f9ada172d63438e7908cea3be62a23b91cca0d35
ankan2810 <22130111@st.hcmuaf.edu.com>
2025-12-10 07:45:07 +0700

    Them file final test

---
+++ cypress/e2e/final_test.cy.js
@@ -0,0 +1,7 @@
+// --- Import file của các thành viên ---
+import './21130033_PhamHaiDuong_Lab7.cy.js' 
+import './21130321_DuongGiaDung_Lab7.cy.js'
+import './21130329_NguyenQuyDuong_Lab7.cy.js'
+import './22130025_PhamThaiBao_Lab7.cy.js'
+import './22130111_DoAnKhang_Lab7/CPS_Store_Filter_and_Search.cy.js' 
+import './22130111_DoAnKhang_Lab7/CPS_Store_Interaction.cy.js'
